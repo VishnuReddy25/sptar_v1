5ba7b701ef12202a6bbaf067670652f2e7dd6f44
VishnuReddy25 <vishnu2522004@gmail.com>
2025-09-25 05:35:13 +0000

    dpr with sptar evaluation done

--- zhiyuan/dpr_eval.py
+++ zhiyuan/dpr_eval.py
@@ -10,24 +10,24 @@ def multirun(args):
         print(f"GPU {arg_dict['gpu_id']} Training: {arg_dict['dataset_name']} on {exp}")
 
         # Uncomment below if training is needed
-        # if arg_dict["version"] == "v1":
-        #     train_command = [
-        #         "python", "zhiyuan/retriever/dpr/train/train_sbert.py",
-        #         "--dataset_name", arg_dict["dataset_name"],
-        #         "--train_num", str(arg_dict["train_num"]),
-        #         "--weak_num", arg_dict["weak_num"],
-        #         "--exp_name", exp
-        #     ]
-        # elif arg_dict["version"] == "v2":
-        #     train_command = [
-        #         "python", "zhiyuan/retriever/dpr/train/train_sbert_BM25_hardnegs.py",
-        #         "--dataset_name", arg_dict["dataset_name"],
-        #         "--train_num", str(arg_dict["train_num"]),
-        #         "--weak_num", arg_dict["weak_num"],
-        #         "--exp_name", exp
-        #     ]
-        # print("Running training command:", " ".join(train_command))
-        # subprocess.call(train_command)
+        if arg_dict["version"] == "v1":
+            train_command = [
+                "python", "zhiyuan/retriever/dpr/train/train_sbert.py",
+                "--dataset_name", arg_dict["dataset_name"],
+                "--train_num", str(arg_dict["train_num"]),
+                "--weak_num", arg_dict["weak_num"],
+                "--exp_name", exp
+            ]
+        elif arg_dict["version"] == "v2":
+            train_command = [
+                "python", "zhiyuan/retriever/dpr/train/train_sbert_BM25_hardnegs.py",
+                "--dataset_name", arg_dict["dataset_name"],
+                "--train_num", str(arg_dict["train_num"]),
+                "--weak_num", arg_dict["weak_num"],
+                "--exp_name", exp
+            ]
+        print("Running training command:", " ".join(train_command))
+        subprocess.call(train_command)
 
         # Always run evaluation
         eval_command = [
